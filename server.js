bb1bdd73fbcab6941c7495e131790d41ed85ba2f
Wesley Handy <wesleylhandy@gmail.com>
2017-09-23 13:12:41 -0400

    add node server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+// Specify the port.
+var port = process.env.PORT || 3000;
+
+app.get('/base', function(req, res) {
+  res.sendFile(path.join(__dirname, 'doc.geojson'));
+});
+
+app.get('/', function(req, res) {
+ res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, function(){
+	console.log('app is listening on ', port);
+});
